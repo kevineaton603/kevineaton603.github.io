eb1d4b07f0ef3b605f4f13077e818fc716d491c1
Kevin Eaton <keaton@dealerpolicy.com>
2021-07-01 09:34:53 -0400

    Full Name

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Kevin Eaton
+            Kevin S. Eaton
           </a>
           <span>, </span>
           <a
